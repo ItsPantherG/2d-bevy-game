e15cbb8a936051d17918a6dc547840f4add670b4
ItsPantherG <matsvandommelen1337@gmail.com>
2023-04-14 21:36:47 +0200

    jumping works

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,5 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy_rapier2d::prelude::*;
@@ -6,28 +8,28 @@ fn main() {
     App::new()
         .add_state::<MovementState>()
         .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
+        // .add_plugin(RapierDebugRenderPlugin::default())
         .init_resource::<JumpTimer>()
         .add_startup_system(setup)
         .add_system(player_move)
         .add_system(jump)
         .add_system(jump_timer_start.in_set(OnUpdate(MovementState::Jumping)))
-        .add_system(gravity)
         .run()
 }
 
 pub const PLAYER_SPEED: f32 = 300.0;
 pub const GRAVITY: f32 = 4250.;
+pub const JUMP_STRENGTH: f32 = 5000.0;
+pub const GRAVITY_STRENGTH: f32 = -1000.0;
 
 #[derive(Component)]
 pub struct Camera;
 
 #[derive(Component)]
 pub struct Player {}
-
-#[derive(Component)]
-pub struct Velocity(pub(crate) Vec2);
 
 #[derive(Resource)]
 pub struct JumpTimer {
@@ -58,7 +60,7 @@ pub fn setup(
     ));
 
     for i in 0..3 {
-        cmds.spawn(Collider::cuboid(227.5, 40.0))
+        cmds.spawn(Collider::cuboid(217.5, 40.0))
             .insert(SpriteBundle {
                 transform: Transform::from_xyz(0.0 + (i as f32 * 430.0), 0.0, 1.0),
                 texture: asset_server.load("sprites/ground.png"),
@@ -69,6 +71,28 @@ pub fn setup(
                 ..default()
             });
     }
+
+    cmds.spawn(Collider::cuboid(217.5, 40.0))
+        .insert(SpriteBundle {
+            transform: Transform::from_xyz(200.0, 250.0, 1.0),
+            texture: asset_server.load("sprites/ground.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(455.0, 100.0)),
+                ..default()
+            },
+            ..default()
+        });
+
+    cmds.spawn(Collider::cuboid(217.5, 40.0))
+        .insert(SpriteBundle {
+            transform: Transform::from_xyz(800.0, 200.0, 1.0),
+            texture: asset_server.load("sprites/ground.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(455.0, 100.0)),
+                ..default()
+            },
+            ..default()
+        });
 
     cmds.spawn((
         SpriteBundle {
@@ -83,26 +107,15 @@ pub fn setup(
         Player {},
     ))
     .insert(KinematicCharacterController {
-        snap_to_ground: Some(CharacterLength::Absolute(0.5)),
+        up: Vec2::Y,
+        snap_to_ground: Some(CharacterLength::Relative(0.2)),
+        min_slope_slide_angle: 50.0_f32.to_radians(),
         ..default()
     })
     .insert(RigidBody::KinematicPositionBased)
     .insert(Collider::capsule_x(20.0, 20.0))
     .insert(LockedAxes::ROTATION_LOCKED)
-    .insert(Velocity(Vec2::ZERO))
     .insert(GravityScale(5.0));
-}
-
-pub fn gravity(
-    time: Res<Time>,
-    current_state: Res<State<MovementState>>,
-    mut player_query: Query<&mut Velocity, With<Player>>,
-) {
-    if let Ok(mut velocity) = player_query.get_single_mut() {
-        if current_state.0 == MovementState::Falling {
-            velocity.0.y = GRAVITY * time.delta_seconds()
-        }
-    }
 }
 
 pub fn player_move(
@@ -115,10 +128,7 @@ pub fn player_move(
     for mut controller in controllers.iter_mut() {
         let mut direction = Vec2::ZERO;
 
-        if current_state.0 == MovementState::Falling {
-            direction += Vec2::new(0.0, -1.0)
-        }
-
+        // Left Right
         if keyboard_input.pressed(KeyCode::A) {
             direction += Vec2::new(-1.0, 0.0)
         }
@@ -126,13 +136,21 @@ pub fn player_move(
             direction += Vec2::new(1.0, 0.0);
         }
 
+        // Normalize and set player speed
         if direction.length() > 0.0 {
-            direction = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_seconds()
+            direction = direction.normalize_or_zero() * PLAYER_SPEED
         }
 
+        // set jump speed
         if current_state.0 == MovementState::Jumping {
-            direction += Vec2::new(0.0, 1.0);
+            direction += Vec2::new(0.0, jump_timer.timer.remaining_secs()) * JUMP_STRENGTH
         }
+
+        // Gravity
+        direction += Vec2::new(0.0, 1.0 * GRAVITY_STRENGTH);
+
+        //
+        direction = direction * time.delta_seconds();
 
         controller.translation = Some(direction)
     }
@@ -147,22 +165,43 @@ pub fn jump(
     current_state: Res<State<MovementState>>,
     mut next_movement_state: ResMut<NextState<MovementState>>,
     keyboard_input: Res<Input<KeyCode>>,
-    jump_timer: Res<JumpTimer>,
+    mut jump_timer: ResMut<JumpTimer>,
 ) {
     for output in controllers.iter() {
-        if keyboard_input.just_pressed(KeyCode::Space) && current_state.0 == MovementState::Idle {
+        if current_state.0 == MovementState::Jumping
+            && f32::trunc(output.effective_translation[1] * 100.0) / 100.0
+                != f32::trunc(output.desired_translation[1] * 100.0) / 100.0
+            && (output.effective_translation[1] > 0.01 || output.effective_translation[1] < -0.01)
+        {
+            next_movement_state.set(MovementState::Falling);
+            jump_timer.timer.reset();
+            println!("State to Falling");
+        }
+
+        if keyboard_input.just_pressed(KeyCode::Space)
+            && current_state.0 == MovementState::Idle
+            && output.grounded
+        {
             println!("State to Jumping");
             next_movement_state.set(MovementState::Jumping);
         }
 
-        if jump_timer.timer.finished() && current_state.0 == MovementState::Jumping {
+        if jump_timer.timer.just_finished() && current_state.0 == MovementState::Jumping {
             println!("State to Falling");
             next_movement_state.set(MovementState::Falling)
         }
 
-        if output.grounded && current_state.0 != MovementState::Idle {
+        if output.grounded
+            && current_state.0 != MovementState::Idle
+            && current_state.0 != MovementState::Jumping
+        {
             next_movement_state.set(MovementState::Idle);
             println!("State to Idle");
+        }
+
+        if output.grounded == false && current_state.0 == MovementState::Idle {
+            next_movement_state.set(MovementState::Falling);
+            println!("State to Falling")
         }
     }
 }
