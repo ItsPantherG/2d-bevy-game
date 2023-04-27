b9704109f4e3ee8b0de3f2605eadc62b4eeab333
ItsPantherG <matsvandommelen1337@gmail.com>
2023-04-27 20:31:26 +0200

    .

--- game/src/mainmenu/mod.rs
+++ game/src/mainmenu/mod.rs
@@ -2,6 +2,7 @@ mod systems;
 
 use bevy::prelude::*;
 
+
 use crate::GameState;
 
 use systems::*;
@@ -10,6 +11,7 @@ pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(start_game_on_click.in_set(OnUpdate(GameState::MainMenu)));
+        app
+            .add_system(start_game_on_click.in_set(OnUpdate(GameState::MainMenu)));
     }
 }
